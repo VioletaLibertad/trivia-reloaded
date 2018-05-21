f94c029be42cf2163603ae6181e55737eba74acd
Violeta <morada.libertad@gmail.com>
2018-05-21 18:06:06 -0400

    Segunda pregunta ready

--- main.js
+++ main.js
@@ -11,58 +11,14 @@ function submit1(questionOne) {
             mensaje.innerHTML = "En " + userAnswerOne + " no hay Laboratoria";
             }
 }
-/*
 function submit2(questionTwo) {
-    var userAnswer2 = document.getElementById("q2").value;
-    if (userAnswer2.toLowerCase() == "no") {
-        document.getElementById("rightAnswersText").innerHTML =
-            document.getElementById("rightAnswersText").innerHTML +
-            "<div class='answer'>" +
-            "<p>No hay laboratoria en concepción</p>"
-        "</div>";
-    } else {
-        document.getElementById("wrongAnswersText").innerHTML =
-            document.getElementById("wrongAnswersText").innerHTML +
-            "<div class='answer'>" +
-            "<p>No hay laboratoria en concepción</p>"
-        "</div>";
-}
-
-
-
-/*    var wantToPlay = prompt("¿quieres jugar? s/n");
-    if (wantToPlay.toLowerCase() == "s") {
-        var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
-        if (answer1.toLowerCase() == "n") {
-            document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
+    var userAnswerTwo = document.getElementById("q2").value;
+    if (userAnswerTwo.toLowerCase() == "6 meses"
+        || userAnswerTwo.toLowerCase() == "seis meses") {
+        var mensaje = document.getElementById("rightAnswersText");
+        mensaje.innerHTML = userAnswerTwo + " es la duración del programa";
         } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
+            var mensaje = document.getElementById("wrongAnswersText");
+            mensaje.innerHTML = userAnswerTwo + " no es la duración del programa"
         }
-
-        var answer2 = prompt("¿hay laboratoria en concepción? s/n");
-        if (answer2.toLowerCase() == "n") {
-            document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No hay laboratoria en concepción</p>"
-            "</div>";
-        } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No hay laboratoria en concepción</p>"
-            "</div>";
-        }
-    } else {
-        document.getElementById("warningMessage").innerText = "Bueno Chao";
-    }
 }
-*/
