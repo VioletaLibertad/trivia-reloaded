0f4bb1f83231f80b777c56e6915d929cf2a392ad
Violeta <morada.libertad@gmail.com>
2018-05-21 17:43:54 -0400

    Primera pregunta ready

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 function submit1(questionOne) {
-    var userAnswer = document.getElementById("q1").value;
-    if (userAnswer.toLowerCase() == "no") {
-        document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
+    var userAnswerOne = document.getElementById("q1").value;
+    if (userAnswerOne.toLowerCase() == "chile" 
+        || userAnswerOne.toLowerCase() == "peru" 
+        || userAnswerOne.toLowerCase() == "mexico" 
+        || userAnswerOne.toLowerCase() == "brasil") {
+        var mensaje = document.getElementById("rightAnswersText");
+        mensaje.innerHTML = "En " + userAnswerOne + " sí hay Laboratoria";
         } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
-    }
+            var mensaje = document.getElementById("wrongAnswersText");
+            mensaje.innerHTML = "En " + userAnswerOne + " no hay Laboratoria";
+            }
 }
-
+/*
 function submit2(questionTwo) {
     var userAnswer2 = document.getElementById("q2").value;
     if (userAnswer2.toLowerCase() == "no") {
